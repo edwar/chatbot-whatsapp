8032d3e7ae1afb7d6b1420a3ec85a1c1e93581c8
Edwar Amaya Diaz <edwaramayadiaz@gmail.com>
2023-02-26 23:43:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const { createProvider } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const ChatGPTClass = require('./chatgpt.class')
+
+const createBotGPT = async ({ provider, database }) => {
+    return new ChatGPTClass(database, provider)
+}
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBotGPT({
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
